bcc9a4caebb76ce9ee8edf80b10e98b2f3618f09
Nicolas Correa <nicolas.z.correa@gmail.com>
2017-10-15 04:21:46 -0400

    Added signed in state and passed it down for testing

--- src/components/WelcomeBox.js
+++ src/components/WelcomeBox.js
@@ -4,15 +4,36 @@ import esportsImage from '../assets/esportscheck.png';
 
 class WelcomeBox extends Component {
     render() {
+        let descString;
+        if(this.props.signedIn !== true) {
+            descString = "Just how #Esports is your Twitter account? Esports Check™ will perfectly assess your Esports levels using revolutionary software";
+        } else {
+            descString = "Welcome Back!";
+        }
+
         return (
             <div className="WelcomeBox">
+
                 <img src={esportsImage} alt="esports logo" height="135" width="285"/>
-                <div className="WelcomeBox-desc"> Just how #Esports is your Twitter account? Esports Check™ will perfectly assess your Esports levels using revolutionary software </div>
-                <div className="twitter-sign-in noselect"> Sign in &nbsp; <i className="fa fa-twitter fa-lg"></i></div>
-                <div className="twitter-sign-in-desc"> Log in with your twitter account </div>
+                <div className="WelcomeBox-desc"> {descString} </div>
+                { this.props.signedIn !== true ?
+                    <div className="twitter-sign-in noselect"> Sign in &nbsp; <i className="fa fa-twitter fa-lg"></i></div>
+                    : 
+                    null
+                }
+
+                { this.props.signedIn !== true ?
+                    <div className="twitter-sign-in-desc"> Log in with your twitter account </div>
+                    : 
+                    null
+                }
             </div>
         );
     }
 }
+
+WelcomeBox.defaultProps = {
+    signedIn: false
+};
 
 export default WelcomeBox;
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,12 +5,32 @@ import WelcomeBox from '../components/WelcomeBox';
 import Footer from '../components/Footer';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signedIn: false
+    }
+
+    //Function used out of context in interval!
+    this.changeSignInState = this.changeSignInState.bind(this);
+  }
+
+  componentDidMount() {
+    setInterval(this.changeSignInState,1000);
+  }
+
+  changeSignInState() {
+    this.setState(prevState => ({ 
+      signedIn: !prevState.signedIn
+    }));
+  }
 
   render() {
     return (
       <div className="App">
         <NavBar />
-        <WelcomeBox />
+        <WelcomeBox signedIn={this.state.signedIn} />
         <Footer />
       </div>
     );
